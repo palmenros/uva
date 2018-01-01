70eef35973f9b68f67a7243a071691b71236b848
Pedro Palacios Almendros <palmenros@gmail.com>
2018-01-01 17:24:30 +0100

    Solved 146

---
+++ 146.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {	
+	string str;
+	while(getline(cin, str), str != "#") {
+		if(next_permutation(str.begin(), str.end())) {
+			cout << str << endl;
+		} else {
+			cout << "No Successor" << endl;
+		}
+	}
+	return 0;
+}
